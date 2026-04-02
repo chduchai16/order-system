e9ed89a2ef1ee1acba553de11c63a86221c25fa3
=cdhai16 <chuduchai1901@gmail.com>
2026-04-02 11:28:02 +0700

    set time zone cho các service cho phù hợp postgre

--- commonlib/src/main/java/com/example/commonlib/CommonlibApplication.java
+++ commonlib/src/main/java/com/example/commonlib/CommonlibApplication.java
@@ -1,13 +1,6 @@
 package com.example.commonlib;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CommonlibApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CommonlibApplication.class, args);
-	}
-
+public final class CommonlibApplication {
+    private CommonlibApplication() {
+    }
 }
