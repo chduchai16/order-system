f57145b44c6887b8712e92bb3966eb7dacc1670d
=cdhai16 <chuduchai1901@gmail.com>
2026-05-03 16:59:44 +0700

    fix : bỏ test

--- paymentservice/src/test/java/com/example/paymentservice/PaymentserviceApplicationTests.java
+++ paymentservice/src/test/java/com/example/paymentservice/PaymentserviceApplicationTests.java
@@ -1,9 +1,6 @@
 package com.example.paymentservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class PaymentserviceApplicationTests {
 
 	@Test
